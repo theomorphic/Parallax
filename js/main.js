dc781f25e98fa3fe1a710dc77827cc565e99b595
theomorphic <FelixMK@yandex.ru>
2024-06-20 05:09:19 +0500

    init

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+//отслеживание положения курсора
+document.addEventListener("mousemove", e =>{
+	Object.assign(document.documentElement, {
+		style: `
+		--move-x: ${(e.clientX - window.innerWidth / 2) * -.005}deg;
+		--move-y: ${(e.clientY - window.innerHeight / 2) * -.01}deg;
+		`
+	})
+})
